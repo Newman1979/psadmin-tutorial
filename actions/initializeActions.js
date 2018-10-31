bd708563dd504d038e0f8f8c3e7e049d0f8918d1
Michael Newman <newman1979@gmail.com>
2018-10-31 15:27:37 -0600

    Add files via upload

---
+++ actions/initializeActions.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var AuthorApi = require('../api/authorApi');
+
+var InitializeActions = {
+    initApp: function() {
+        Dispatcher.dispatch({
+            actionType: ActionTypes.INITIALIZE,
+            authors: AuthorApi.getAllAuthors()
+        });
+    }
+};
+
+module.exports = InitializeActions;
